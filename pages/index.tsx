59cc9791c611e22ddda034de3cf4eb9602b9f9d1
Andy Birch <andy@outstandy.com>
2019-12-06 11:33:15 -0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Index = () => (
+  <div>
+    <Head>
+      <title>MojoTech Design</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <h1>Home page</h1>
+  </div>
+);
+
+export default Index;
